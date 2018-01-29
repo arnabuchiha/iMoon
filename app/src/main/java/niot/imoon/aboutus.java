9292a41366cf4bff0e4c91c55ffd2665d0de1e1d
arnabuchiha <ad733943@gmail.com>
2018-01-30 01:10:41 +0530

    New Updated

--- app/src/main/java/niot/imoon/aboutus.java
+++ app/src/main/java/niot/imoon/aboutus.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.view.GestureDetector;
@@ -14,8 +15,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.mingle.entity.MenuEntity;
 import com.mingle.sweetpick.DimEffect;
@@ -37,6 +36,7 @@ public class aboutus extends Fragment {
     private insat insat;
     private aboutniot aboutniot;
     private settings settings;
+    ArcMenu arcMenu;
     private data_request data_request;
     FragmentTransaction ft;
 
@@ -54,6 +54,7 @@ public class aboutus extends Fragment {
         aboutniot=new aboutniot();
         data_request=new data_request();
         settings=new settings();
+        arcMenu=(ArcMenu)v.findViewById(R.id.arcMenu);
          //ft= getFragmentManager().beginTransaction();
         //setupViewpager();
 //        mSweetSheet2.show();
@@ -74,11 +75,11 @@ public class aboutus extends Fragment {
         buoy_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                arcMenu.toggleMenu();
                 ft= getFragmentManager().beginTransaction();
                 ft.replace(R.id.container_frag,buoy_status_map);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.addToBackStack(null);
-                buoy_map.setVisibility(View.GONE);
                 ft.commit();
 
             }
@@ -88,11 +89,11 @@ public class aboutus extends Fragment {
         data_req.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                arcMenu.toggleMenu();
                 ft= getFragmentManager().beginTransaction();
                 ft.replace(R.id.container_frag,data_request);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.addToBackStack(null);
-                data_req.setVisibility(View.GONE);
                 ft.commit();
             }
         });
@@ -100,11 +101,11 @@ public class aboutus extends Fragment {
         about_niot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                arcMenu.toggleMenu();
                 ft= getFragmentManager().beginTransaction();
                 ft.replace(R.id.container_frag,aboutniot);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.addToBackStack(null);
-                about_niot.setVisibility(View.GONE);
                 ft.commit();
             }
         });
@@ -112,11 +113,11 @@ public class aboutus extends Fragment {
         imd_forecast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                arcMenu.toggleMenu();
                 ft= getFragmentManager().beginTransaction();
                 ft.replace(R.id.container_frag,insat);
                 ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
                 ft.addToBackStack(null);
-imd_forecast.setVisibility(View.GONE);
                 ft.commit();
             }
         });
@@ -124,124 +125,12 @@ imd_forecast.setVisibility(View.GONE);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-settings.setVisibility(View.GONE);
+                arcMenu.toggleMenu();
                 Intent myIntent = new Intent(aboutus.this.getActivity(), settings.class);
                 startActivity(myIntent);
             }
         });
-
-
-        ////////
-
-/******
-
-
-
-
-        final GestureDetector gesture = new GestureDetector(getActivity(), new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent e) {
-                return false;
-            }
-
-            @Override
-            public void onShowPress(MotionEvent e) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent e) {
-                if(mSweetSheet2.isShow()){
-                    mSweetSheet2.dismiss();
-                }
-                else
-                    mSweetSheet2.show();
-
-                return true;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent e) {
-                mSweetSheet2.toggle();
-
-            }
-
-            @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                return false;
-            }
-        });
-
-
-        v.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                gesture.onTouchEvent(event);
-                return true;            }
-        });
-
-
-        return v;
-
-    }
-
-    private void setupViewpager() {
-
-        mSweetSheet2 = new SweetSheet(rl);
-        mSweetSheet2.setMenuList(R.menu.menu_sweet);
-        mSweetSheet2.setDelegate(new ViewPagerDelegate());
-        mSweetSheet2.setBackgroundEffect(new DimEffect(0.5f));
-        mSweetSheet2.setOnMenuItemClickListener(new SweetSheet.OnMenuItemClickListener() {
-            @Override
-            public boolean onItemClick(int position, MenuEntity menuEntity1) {
-                switch (position){
-                    case 0:
-                        ft.replace(R.id.container_frag,buoy_status_map);
-                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                    break;
-                    case 1:
-                        ft.replace(R.id.container_frag,aboutniot);
-                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                        break;
-                    case 2:
-                        ft.replace(R.id.container_frag,data_request);
-                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                        break;
-                    case 3:
-                        ft.replace(R.id.container_frag,insat);
-                        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                        ft.addToBackStack(null);
-                        ft.commit();
-                        break;
-                    case 4:
-                        Intent myIntent = new Intent(aboutus.this.getActivity(), settings.class);
-                        startActivity(myIntent);
-                        break;
-                }
-
-                Toast.makeText(getContext(), menuEntity1.title + "  " + position, Toast.LENGTH_SHORT).show();
-
-                return true;
-            }
-        });
-    }
-
-    *********/
-
-
-
-
+        
         return v;
 
 }
